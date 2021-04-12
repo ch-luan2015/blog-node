1bb0776e68f17936b65a2c74a86147c6ab05d323
ch-luan2015 <ch.luan2015@gmail.com>
2021-04-12 16:59:26 +0700

    new

--- index.js
+++ index.js
@@ -5,31 +5,40 @@ const ejs = require('ejs')
 const bodyParser = require("body-parser")
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.raw());
 
 
 const BlogPost = require("./models/BlogPost")
+const Singer = require("./models/Singer")
+
 //Connect DataBase
 const mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/SingerDB', { useNewUrlParser: true, useUnifiedTopology: true })
+
 
 app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
-
-
-app.get('/', (req, res) => {
-    res.render('index')
-})
 
 app.get("/posts/new", (req, res) => {
     res.render("create")
 })
 app.post('/posts/store', (req, res) => {
     console.log(req.body)
-    BlogPost.create(req.body, (e, blogpost) => {
+    Singer.create(req.body, (e, singer) => {
         res.redirect('/')
 
     })
+})
+
+
+//fetch data from database : use find({},()=>}|)
+
+app.get('/', async (request, res) => {
+    const SingerCollection = await Singer.find({});
+    res.render('index', {
+        SingerCollection: SingerCollection
+    });
 })
 
 app.get('/about', (req, res) => {
@@ -38,8 +47,12 @@ app.get('/about', (req, res) => {
 app.get('/contact', (req, res) => {
     res.render('contact');
 })
-app.get('/post', (req, res) => {
-    res.render('post')
+app.get('/post/:id', (req, res) => {
+    Singer.findById(req.params.id, function (error, detailSinger) {
+        res.render('post', {
+            detailSinger
+        })
+    })
 })
 
 
---
+++ models/Singer.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//Định kiểu types cho đôi tượng
+const SingerSchema = new Schema({
+    name: String,
+    song: String,
+})
+
+//Tạo model cho kiểu : tên model và dạng types của nó
+const Singer = mongoose.model("singers", SingerSchema);
+
+//Giống export default Name;
+module.exports = Singer;
